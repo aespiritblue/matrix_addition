6e24846da77d6c2fd0c0b6492426e1393ab9ec70
aespiritblue <163371299+aespiritblue@users.noreply.github.com>
2026-03-27 17:18:12 -0400

    Added print2darray()

--- Main.java
+++ Main.java
@@ -16,6 +16,9 @@ threads compared to processes, etcetera,
 and connect these issues to 
 multi-threading.
 
+Multi-threading minimizes resource usage by allowing multiple threads to share the same space in memory. 
+This allows multiple processes to happen when running the program without having to resorting to having to having to "eat up" all the memory.
+It also takes advantage of multicore processors by running threads in parallel without the overhead of creating separate processes, which makes execution of programs more efficient.
 */
 import java.io.IOException;
 import java.io.File;
@@ -25,11 +28,40 @@ public class Main
 {
 	public static void main(String[] args) 
 	{
-		
-		System.out.println();
-		
+		/* Testing print2dArray
+		int[][] testMatrix = 
+        {
+            {5,0,0,0},
+            {0,8,0,0},
+            {0,0,0,0},
+            {0,6,0,0}
+        };
+		print2dArray(testMatrix);
+		*/
 		
 		
 	}
+
+	//taken straight out of my sparse array assignment
+	public static void print2dArray(int[][] matrix)
+	{
+		for(int i = 0; i < matrix.length; i++)
+        {
+            for(int j = 0; j < matrix[0].length; j++)
+            {
+                System.out.printf(matrix[i][j] + "\t");
+            }
+            System.out.println();    
+        }    
+        System.out.println();
+	}
+
+	//converts file to 2d int array
+	public static int[][] matrixFromFile(int rows, int columns, Scanner file_reader)
+	{
+		return null; //TODO: fill this in
+		
+	}
+
 
 }
---
+++ ThreadOperation.java
@@ -0,0 +1,17 @@
+public class ThreadOperation extends Thread
+{
+    private int[][] matrixA;
+    private int[][] matrixB;
+    private int[][] matrixC;
+    private String quadrant;
+
+    public ThreadOperation(int[][] matrixA, int[][] matrixB, int[][] matrixC, String quadrant)
+    {
+
+    }
+
+    public void run()
+    {
+        
+    }
+}
